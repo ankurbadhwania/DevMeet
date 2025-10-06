a6910cf331152a3403a6f5cdd60b19efd1c16a1c
ankurbadhwania <ankurbadhwania@gmail.com>
2025-10-06 08:15:40 +0530

    routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,22 @@
 const express = require("express")
 const app = express();   // creating application
 
-app.use("/test",(req, res) => {
+app.use("/test",(req, res) => {  // order of routes matter
     res.send("hello test")
 })
-app.use((req, res) => {
+app.use("/hello",(req, res) => { 
+    res.send("hello hello")
+})
+app.use("/", (req, res) => {
     res.send("hello from the server")
 })
-app.listen(3000, ()=>{
-    console.log("server started")
+
+app.get("/user", (req, res) =>{
+    console.log("user data fetched");
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`server started, listening to port ${port}`)
 })
  
