5b839b3f8e95c09a2a365b96dff732a2928ef9a0
ankurbadhwania <ankurbadhwania@gmail.com>
2025-10-06 11:48:44 +0530

    using next in route handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,24 @@
 const express = require("express")
 const app = express();   // creating application
 
+
+app.get("/user", (req, res, next) =>{
+    console.log(req.params)
+    console.log("1st done");
+    // res.send("response 1");
+    next();
+},
+(req,res,next) =>{
+    console.log("2nd done");
+    // res.send("response 2");
+    next();
+},
+(req,res,next) =>{
+    res.send("response 3");
+})
+
+
+
 app.get("/user/:userid/:name", (req, res) =>{
     console.log(req.params)
     res.send("user data fetched");
